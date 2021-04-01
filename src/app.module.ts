dad15bfa7f965ad79a3a60836e7f1b99bcd1f52e
Jakub Sukiennik <kuba1207710@gmail.com>
2021-04-01 20:13:28 +0200

    adding docker and postgres, connecting to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      keepConnectionAlive: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
